f53eb98ecdf65ade8fd922538d92eee1db0171b0
developerkhashab <mohammad.khashab.mk@gmail.com>
2021-08-08 22:06:55 -0800

    add models

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { OnInit } from "@angular/core";
+import { Component } from "@angular/core";
+import { IPagination } from "./Models/pagination";
+import { IProduct } from "./Models/product";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
-  title = 'Skinet';
+export class AppComponent implements OnInit {
+  title = "Skinet";
+  products : IProduct[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/products?pageSize=50').subscribe((response : IPagination) =>{
+        this.products = response.data;
+        console.log(response.data);
+    }, error =>{
+        console.log(error);
+    });
+  }
 }
